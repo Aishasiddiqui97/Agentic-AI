420e6838cdcc24a89bb778965284dcf280993801
AishaSiddiqui <aishaanjumsiddiqui97@gmail.com>
2025-06-05 00:38:11 +0500

    main.py

--- Heyworld/main.py
+++ Heyworld/main.py
@@ -1,55 +1,8 @@
 # def main():
 #     print("Hello from heyworld!")
-
 # print("__name__>>>>",__name__)
-
 # if __name__ == "__main__": #name guard
 #    main()
-
-
-# agent =Agent(name="Assistant",instructions="You are a helpfull assistant")  #class
-
-# result = Runner.run_sync(starting_agent=agent, input="Write a haiku about recursion in programming.")
-
-# print(result.final_output)
-
-# # Code within the code,
-# # Functions calling themselves,
-# # Infinite loop's dance.
-#if __name__ == "__main__": #name guard
-#    main()
-
-
-
-
-# from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
-# from agents.run import RunConfig
-
-# gemini_api_key = "AIzaSyClKLOVc-o6awnksGP3S_f3VF1iu08oxtw"
-
-# #Reference: https://ai.google.dev/gemini-api/docs/openai
-# client = AsyncOpenAI(
-#     api_key=gemini_api_key,
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-# )
-
-# model = OpenAIChatCompletionsModel(
-#     model="gemini-2.0-flash",
-#     openai_client=client
-# )
-
-# config = RunConfig(
-#     model=model,
-#     model_provider=client,
-#     tracing_disabled=True
-# )
-
-# agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant")
-
-# result = Runner.run_sync(starting_agent=agent, input="tu jaa ray baba", run_config=config)
-
-# print(result.final_output)
-
 
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 from agents.run import RunConfig
@@ -78,3 +31,9 @@ agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant
 result = Runner.run_sync(starting_agent=agent, input="Har k jeetna wala ko bazigar khta h", run_config=config)
 
 print(result.final_output)
+
+# # Code within the code,
+# # Functions calling themselves,
+# # Infinite loop's dance.
+# if __name__ == "__main__":  # name guard
+#    main()
