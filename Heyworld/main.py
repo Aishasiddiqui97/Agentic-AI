55d44bb2ed5f1e84adff139453ff29ee7f4c24cb
Aisha.A.Siddiqui <aishaanjumsiddiqui97@gmail.com>
2025-07-25 16:22:17 +0500

    Add files via upload

--- Heyworld/main.py
+++ Heyworld/main.py
@@ -1,6 +1,8 @@
 # def main():
 #     print("Hello from heyworld!")
+
 # print("__name__>>>>",__name__)
+
 # if __name__ == "__main__": #name guard
 #    main()
 
@@ -23,17 +25,55 @@ model = OpenAIChatCompletionsModel(
 config = RunConfig(
     model=model,
     model_provider=client,
-    tracing_disabled=True
+    tracing_disabled=True 
 )
 
 agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant")
 
-result = Runner.run_sync(starting_agent=agent, input="Har k jeetna wala ko bazigar khta h", run_config=config)
+result = Runner.run_sync(starting_agent=agent, input="Asalm o alikum myself AISHA", run_config=config)
 
 print(result.final_output)
 
-# # Code within the code,
-# # Functions calling themselves,
-# # Infinite loop's dance.
-# if __name__ == "__main__":  # name guard
-#    main()
+# Code within the code,
+# Functions calling themselves,
+# Infinite loop's dance.
+# if __name__ == "__main__": #name guard
+#   main()
+
+
+
+# #golobal
+
+# import asyncio
+# from openai import AsyncOpenAI
+# from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+
+# gemini_api_key = "AIzaSyDhD_WyAAS7eXi44gz1dvaWmqYlL9U_ah0"
+
+# #Reference: https://ai.google.dev/gemini-api/docs/openai
+# client = AsyncOpenAI(
+#     api_key=gemini_api_key,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+# )
+
+# set_tracing_disabled(disabled=True)
+
+# async def main():
+#     # This agent will use the custom LLM provider
+#     agent = Agent(
+#         name="Assistant",
+#         instructions="You only respond in haikus.",
+#         model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client),
+#     )
+
+#     result = await Runner.run_sync(
+#         agent,
+#         "Tell me about recursion in programming.")
+#     print(result.final_output)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+
+
